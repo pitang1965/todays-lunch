a51781604b3eac1deb34d9e1507f362fe4c0a8fb
Shoji Makino <pitang1965@gmail.com>
2022-02-21 00:54:51 +0900

    Add utility functions about date

---
+++ src/lib/timeUtile.ts
@@ -0,0 +1,18 @@
+function zeroPad(i: number) {
+  return i < 10 ? '0' + i : i;
+}
+
+function getDayText(index: number) {
+  const days = ['日', '月', '火', '水', '木', '金', '土'];
+  return days[index];
+}
+
+export function getStringFromDate(date: Date, withDayKanji = false) {
+  const YYYY = date.getFullYear();
+  const MM = date.getMonth() + 1;
+  const DD = date.getDate();
+  const hh = date.getHours();
+  const mm = date.getMinutes();
+  const day = withDayKanji ? ` (${getDayText(date.getDay())})` : '';
+  return (`${YYYY}-${zeroPad(MM)}-${zeroPad(DD)} ${zeroPad(hh)}:${zeroPad(mm)}${day}`);
+}
