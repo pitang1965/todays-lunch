27001f99e10b85c0d68dfc406a07ce1e91bea044
Shoji Makino <pitang1965@gmail.com>
2022-02-26 10:30:44 +0900

    Change the order of disclosures

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -11,23 +11,6 @@ export function Instructions() {
           {({ open }) => (
             <>
               <Disclosure.Button className='flex justify-between py-2 px-4 w-full text-sm font-medium text-left text-purple-900 bg-purple-100 hover:bg-purple-200 rounded-full focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75'>
-                <span>どのようなメールが送られますか?</span>
-                <ChevronUpIcon
-                  className={`${
-                    open ? 'transform rotate-180' : ''
-                  } w-5 h-5 text-purple-500`}
-                />
-              </Disclosure.Button>
-              <Disclosure.Panel className='px-4 pt-4 pb-2 text-sm text-gray-500'>
-                自分にCcされてきたメールを確認願います。
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-        <Disclosure as='div' className='mt-2'>
-          {({ open }) => (
-            <>
-              <Disclosure.Button className='flex justify-between py-2 px-4 w-full text-sm font-medium text-left text-purple-900 bg-purple-100 hover:bg-purple-200 rounded-full focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75'>
                 <span>注意その１</span>
                 <ChevronUpIcon
                   className={`${
@@ -71,6 +54,23 @@ export function Instructions() {
               </Disclosure.Button>
               <Disclosure.Panel className='px-4 pt-4 pb-2 text-sm text-gray-500'>
                 平田食堂にメールするなりして対応してください。
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as='div' className='mt-2'>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className='flex justify-between py-2 px-4 w-full text-sm font-medium text-left text-purple-900 bg-purple-100 hover:bg-purple-200 rounded-full focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75'>
+                <span>どのようなメールが送られますか?</span>
+                <ChevronUpIcon
+                  className={`${
+                    open ? 'transform rotate-180' : ''
+                  } w-5 h-5 text-purple-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className='px-4 pt-4 pb-2 text-sm text-gray-500'>
+                自分にCcされてきたメールを確認願います。
               </Disclosure.Panel>
             </>
           )}
