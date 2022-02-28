fa16453208f0326b510dca8360763b1296740b0b
Shoji Makino <pitang1965@gmail.com>
2022-02-28 21:51:04 +0900

    Fix: always sending e-mail to ORDER_MAIL_TO_FOR_TEST

--- src/pages/api/sendOrderMail.ts
+++ src/pages/api/sendOrderMail.ts
@@ -6,7 +6,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const emailTo = process.env.NEXT_PUBLIC_TEST_MODE
+  const emailTo = process.env.NEXT_PUBLIC_TEST_MODE === 'true'
     ? process.env.ORDER_MAIL_TO_FOR_TEST
     : process.env.ORDER_MAIL_TO;
   sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
