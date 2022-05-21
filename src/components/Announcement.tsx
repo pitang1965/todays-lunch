a123569947c45b7fc07bf5c5b408f320b499f3c5
Shoji Makino <pitang1965@gmail.com>
2022-05-21 17:29:53 +0900

    Change announcement

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export const Announcement = () => {
   return (
-    <div className='py-3 px-4 mb-2 text-white bg-green-600'>
+    <div className='py-3 px-4 mb-2 text-white bg-blue-600'>
       <p className='text-sm font-medium text-center'>
-        メニュー、ライス、食事開始時間が保存されない不具合を修正したつもりです（汗）。
+        お弁当アイコンは中学1年生に作ってもらいました。
       </p>
     </div>
   );
