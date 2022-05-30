fbe981eea6c618001c3aca36d6d1fd7e71235100
Shoji Makino <pitang1965@gmail.com>
2022-05-30 21:35:24 +0900

    Try to fix a bug about storing order in OrderTable

--- src/pages/api/order/createAirtableRecord.ts
+++ src/pages/api/order/createAirtableRecord.ts
@@ -1,42 +1,38 @@
 const Airtable = require('airtable');
 import type { OrderInfo } from './orderInfo';
 
-export async function createAirtableRecord(orderInfo: OrderInfo): Promise<any> {
+export const createAirtableRecord = async (orderInfo: OrderInfo) =>  {
   console.table(orderInfo);
 
   const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
     process.env.AIRTABLE_BASE_ID
   );
 
-  // 10レコードまで作成できるが1つだけ作成
-  const tableName = process.env.NEXT_PUBLIC_TEST_MODE === 'true' ? 'OrderTableForTest' : 'OrderTable';
-  base(tableName).create(
-    [
+  const tableName =
+    process.env.NEXT_PUBLIC_TEST_MODE === 'true'
+      ? 'OrderTableForTest'
+      : 'OrderTable';
+  
+  try {
+    base(tableName).create([
       {
         fields: {
-          'mailFrom': orderInfo.mailFrom,
-          'date': orderInfo.date,
-          'timeFrom': orderInfo.timeFrom,
-          'department': orderInfo.department,
-          'name': orderInfo.name,
-          'telephoneNumber': orderInfo.telephoneNumber,
-          'employeeNumber': orderInfo.employeeNumber,
-          'menu': orderInfo.menu,
-          'rice': orderInfo.rice,
-          'comment': orderInfo.comment,
+          mailFrom: orderInfo.mailFrom,
+          date: orderInfo.date,
+          timeFrom: orderInfo.timeFrom,
+          department: orderInfo.department,
+          name: orderInfo.name,
+          telephoneNumber: orderInfo.telephoneNumber,
+          employeeNumber: orderInfo.employeeNumber,
+          menu: orderInfo.menu,
+          rice: orderInfo.rice,
+          comment: orderInfo.comment,
         },
       },
-    ],
-    function (err: any, records: any[]) {
-      if (err) {
-        console.error(err);
-        return err;
-      }
-      records.forEach(function (record) {
-        console.log(record.getId());
-      });
-    }
-  );
-
-  return 'OK';
+    ]);
+    return ('OK');
+  } catch (error) {
+    console.error(error);
+    return ('NG');
+  }
 }
