18d2a6dcb1053023d8c20dda5d1b265cfe65a23e
Shoji Makino <pitang1965@gmail.com>
2022-06-04 10:13:48 +0900

    Update announcement

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 
 export const Announcement = () => {
   return (
-    <div className='py-3 px-4 mb-2 text-white bg-purple-600'>
-      <p className='text-sm font-medium text-center'>
-        ダブってタップしても２つ注文入らないようにしました。
+    <div className='py-3 px-4 mb-2 text-sm font-medium text-center text-white bg-red-600'>
+      <p>お弁当のアイコンはうちの中1が作りました。 </p>
+      <p className='underline'>
+        <Link href='/about'>
+          同僚に本アプリを教える場合はQRコードをご使用ください。
+        </Link>
       </p>
     </div>
   );
