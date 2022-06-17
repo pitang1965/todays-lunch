2e9201c048986fe343fb9f647334df4e151399e5
Shoji Makino <pitang1965@gmail.com>
2022-06-18 01:12:19 +0900

    Change Annoucement

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
 
 export const Announcement = () => {
   return (
-    <div className='py-3 px-4 mb-2 text-sm font-medium text-center text-white bg-red-600'>
-      <p className='underline'>
-        <Link href='/about'>
-          同僚に本アプリを教える場合はQRコードをご使用ください。
-        </Link>
-      </p>
+    <div className='py-3 px-4 mb-2 text-sm font-medium text-center text-white bg-green-600 '>
+      6月20日より麺類は夏メニューになります。詳しくは[詳細]ボタンからどうぞ。
     </div>
   );
 };
