44fe7595fe3ae92993367b9b92336d1349a232d3
Pitang1965 <47315420+pitang1965@users.noreply.github.com>
2022-06-24 16:00:11 +0900

    Update Annoucement.tsx
    Update Annoucement.tsx

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Announcement = () => {
   return (
-    <div className='py-3 px-4 mb-2 text-sm font-medium text-center text-white bg-green-600 '>
-      6月20日より麺類は夏メニューになります。詳しくは[詳細]ボタンからどうぞ。
+    <div className='py-3 px-4 mb-2 text-sm font-medium text-center text-white bg-red-600 '>
+      6月27日よりお味噌汁は各自で用意する方式となります。夏メニューもご利用ください。
     </div>
   );
 };
