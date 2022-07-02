b4029fab50b074d6e5aed0593508084fd59579a3
Shoji Makino <pitang1965@gmail.com>
2022-07-02 21:42:46 +0900

    Update Annoucement.tsx

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Announcement = () => {
   return (
     <div className='py-3 px-4 mb-2 text-sm font-medium text-center text-white bg-green-600 '>
-      6月20日より麺類は夏メニューになります。詳しくは[詳細]ボタンからどうぞ。
+      水曜日と金曜日は限定弁当があります。詳しくは[詳細]ボタンからどうぞ。
     </div>
   );
 };
