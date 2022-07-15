5d79b5815d6c5e0530547c5f432a8e3ebd05e97e
Shoji Makino <pitang1965@gmail.com>
2022-07-15 22:42:14 +0900

    Change announcement

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Announcement = () => {
   return (
-    <div className='py-3 px-4 mb-2 text-sm font-medium text-center text-white bg-green-600 '>
-      水曜日と金曜日は限定弁当があります。詳しくは[詳細]ボタンからどうぞ。
+    <div className='mb-2 bg-orange-600 py-3 px-4 text-center text-sm font-medium text-white '>
+      複数回注文ボタンを押すと複数注文が入ってしまう不具合を修正しました。m(_ _)m
     </div>
   );
 };
