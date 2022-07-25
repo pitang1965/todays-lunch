cffccb4726e80a6e059b3f76282635dfb0fab361
Pitang1965 <47315420+pitang1965@users.noreply.github.com>
2022-07-25 15:55:13 +0900

    Update Announcement.tsx

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Announcement = () => {
   return (
-    <div className='mb-2 bg-orange-600 py-3 px-4 text-center text-sm font-medium text-white '>
-      複数回注文ボタンを押すと複数注文が入ってしまう不具合を修正しました。m(_ _)m
+    <div className='mb-2 bg-red-600 py-3 px-4 text-center text-sm font-medium text-white '>
+      お疲れ様です。毎日熱いですね。
     </div>
   );
 };
